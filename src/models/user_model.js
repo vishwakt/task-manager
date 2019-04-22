c49d1f392930a8b091da4374fc82da86bf015c06
Vishwak <vishwakt@gmail.com>
2019-04-22 14:50:01 -0400

    Refactored queries to separate files

--- src/models/user_model.js
+++ src/models/user_model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-const User_model = mongoose.model('User_model', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -39,5 +39,15 @@ const User_model = mongoose.model('User_model', {
         }
     }
 })
+
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    console.log("Just before saving")
+    
+    next()
+})
+
+const User_model = mongoose.model('User_model', userSchema)
 
 module.exports = User_model
