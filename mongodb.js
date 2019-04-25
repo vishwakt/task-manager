17abf1fcf0f43a2f7e561cf2f57fee3f14d2dc2f
Vishwak <vishwakt@gmail.com>
2019-04-24 23:05:48 -0400

    Added config file

--- mongodb.js
+++ mongodb.js
@@ -1,35 +1,3 @@
-// // CRUD operations
-// //
-// // const { MongoClient, ObjectID} = require('mongodb')
-// //
-// // const connectionURL = 'mongodb://127.0.0.1:27017'
-// // const databaseName = 'task-manager'
-// //
-// //
-// // MongoClient.connect(connectionURL, { "useNewUrlParser": true}, (error, client) => {
-// //     if (error) {
-// //         return console.log('Unable to connect to the database')
-// //     }
-// //
-// //     const db = client.db(databaseName)
-// //
-// //     // db.collection('users').deleteMany({
-// //     //     age: 18
-// //     // }).then((result) => {
-// //     //     console.log(result.deletedCount)
-// //     // }).catch((error) => {
-// //     //     console.log(error)
-// //     // })
-// //
-// //     // db.collection('tasks').deleteOne({
-// //     //     description: 'Eat'
-// //     // }).then((result) => {
-// //     //     console.log(result.deletedCount)
-// //     // }).catch((error) => {
-// //     //     console.log(error)
-// //     // })
-// // })
-
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 
--- src/models/user_model.js
+++ src/models/user_model.js
@@ -65,7 +65,7 @@ userSchema.methods.toJSON = function () {
 
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = await jwt.sign({ _id: user._id.toString() }, 'thisisasecret')
+    const token = await jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET)
 
     user.tokens = user.tokens.concat({token})
     await user.save()
